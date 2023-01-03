f3f12ec0d33e02dc31e175467a0c63f94037c013
santiago novoa <santiago.novoa@aviatecnologia.com>
2023-01-03 11:06:51 -0500

    Configuracion del deploy a Github

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -30,3 +30,48 @@ const useInitialState=()=>{
 } 
 
 export default useInitialState;
+
+
+
+// const addToCart = () => {
+        
+//     setContexto({...contexto.cart, carrito: true, cart: [{adultos: cantidadAdulto, niños: contadorChild, tipoHabitacion: tipoHabitacion, precio: parseInt(precio) * contadorAdultos + parseInt(informacionChild.Amount) * contadorChild, tipoMoneda: tipoMoneda}]})
+    
+//     //   setCarrito({
+//     //         cart: [...carrito.cart, {adultos: cantidadAdulto, niños: contadorChild, tipoHabitacion: tipoHabitacion, precio: parseInt(precio) * contadorAdultos + parseInt(precio) * contadorChild, tipoMoneda: tipoMoneda}],
+//     //       });
+//         console.log(carrito);
+//         console.log(contexto.cart);
+// };
+
+
+// /* 16 */ const [cotizacion, setCotizacion] = useState([]);
+
+// /* 27 */ const handleCotizar = () => {
+//     setContexto({ ...contexto, /* informacionDetalle: true, */ personas: personas, tipoHabitacion: tipoHabitacion, precio: parseInt(precio) * cantidadAdulto, tipoMoneda: tipoMoneda });
+// }
+
+// const addToCart = () => {
+    
+//     setCotizacion(...cotizacion, {adultos: cantidadAdulto, niños: contadorChild, tipoHabitacion: tipoHabitacion, precio: precioTotal, tipoMoneda: tipoMoneda})
+//     console.log(cotizacion)
+//     setContexto({...contexto, cotizacion})
+//     console.log(contexto);
+    
+// };
+
+// /* 52 */ const precioTotal = `${formatterPeso.format((parseInt(precio) * cantidadAdulto) +
+//     (informacionChild !== undefined && informacionChild ? (parseInt(informacionChild?.Amount) * contadorChild) : 0) +
+//     (informacionAdultoAdicional !== undefined && informacionAdultoAdicional ? (parseInt(informacionAdultoAdicional.Amount) * nochesAdicionales * cantidadAdulto) : 0) +
+//     (informacionChildAdicional !== undefined && informacionChildAdicional ? (parseInt(informacionChildAdicional.Amount) * nochesAdicionales * cantidadChild) : 0))} ${tipoMoneda}`
+
+// /* 165 */ <button className='btn-secondary_submit px-14 mb-4' onClick={addToCart}>Agregar</button>
+
+
+
+
+
+
+/* 14 */ // console.log(informacionPackages); acomodacion
+
+
